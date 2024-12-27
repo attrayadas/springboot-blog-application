e8fd391ad097e2993fe33fee8cc0bb0d61c3bf56
attraya <attrayaghoshdas@gmail.com>
2024-12-27 13:33:53 +0530

    WebApp Layout using Thymleaf & WebJars

--- src/main/java/com/attraya/blog/BlogApplication.java
+++ src/main/java/com/attraya/blog/BlogApplication.java
@@ -2,12 +2,14 @@ package com.attraya.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class SpringbootBlogApplication {
+public class BlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBlogApplication.class, args);
+		SpringApplication.run(BlogApplication.class, args);
 	}
 
 }
